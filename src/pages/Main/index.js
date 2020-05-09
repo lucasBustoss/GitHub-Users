e15a0985a4f72994fda849ca442d4f81d01e31d3
lucasbustoss <lbustos.silva@icloud.com>
2020-05-09 13:37:24 -0300

    Iniciando projeto usando ReactNative

---
+++ src/pages/Main/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleSubmit = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalizer="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditind={this.handleSubmit}
+          />
+          <SubmitButton onPress={this.handleSubmit}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Usuarios',
+};
